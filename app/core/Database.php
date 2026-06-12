49ac88704101b3be3fb8d86318952bc8e6d66522
JOSE OBIANG <joseobiang57@gmail.com>
2026-06-12 19:42:12 +0200

    fix sql_mode init command

--- app/core/Database.php
+++ app/core/Database.php
@@ -8,8 +8,9 @@ class Database {
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES => false,
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET SESSION sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''))",
         ];
-        try { $this->pdo = new PDO($dsn, DB_USER, DB_PASS, $options); $this->pdo->exec("SET SESSION sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''))"); }
+        try { $this->pdo = new PDO($dsn, DB_USER, DB_PASS, $options); }
         catch (PDOException $e) { die('Erreur connexion BDD : ' . $e->getMessage()); }
     }
     public static function getInstance() {
